3cd30fc0ee7d99a4f224ddc3f029b91051c40194
HOJUN2404 <rlaghwns1995@naver.com>
2022-04-24 22:08:54 +0900

    Create admin id (django.admin)

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from .models import Fcuser
 
 # Create your views here.
 
@@ -9,5 +12,24 @@ def register(request):
         return render(request, 'register.html')
 
     elif request.method =='POST':
-        return render(request, 'register.html')
-    
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username=username,
+                password=make_password(password)
+            )
+
+            fcuser.save()
+
+        return render(request, 'register.html', res_data)
+
+
