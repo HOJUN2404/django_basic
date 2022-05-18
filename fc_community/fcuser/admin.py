10df9cb7e8c31bc5b2f23902ac78e5d4713ccc0c
HOJUN2404 <rlaghwns1995@naver.com>
2022-05-19 00:08:40 +0900

    Admin 수정

--- fc_community/fcuser/admin.py
+++ fc_community/fcuser/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Fcuser
+
 # Register your models here.
 
+
 class FcuserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'useremail', 'password')
+    list_display = ('username', 'password')
+
 
 admin.site.register(Fcuser, FcuserAdmin)
-
